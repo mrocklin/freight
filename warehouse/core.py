3b97cfa482f1ba58c556ed0214722bfa2a4f3b4c
Matthew Rocklin <mrocklin@gmail.com>
2015-03-14 10:27:49 -0700

    add Warehouse MutableMapping

---
+++ warehouse/core.py
@@ -0,0 +1,95 @@
+from zmqompute import ComputeNode
+import zmq
+from redis import Redis
+from collections import MutableMapping
+
+context = zmq.Context()
+
+class Warehouse(MutableMapping):
+    """ A Hosted dictionary with links to other dictionaries
+
+    A warehouse is a combination of the following
+
+    * data - a local MutableMapping
+    * local_server - a local ComputeNode serving data from self.data
+    * redis_db - a connection to a Redis instance mapping keys to urls of
+                 Warehouses that hold values of those keys
+
+    Several warehouses act in concert to provide a distributed bulk key-value
+    storage solution.
+    """
+    def __init__(self, data=None, redis_db=None, host=None, port=None):
+        self.data = data if data is not None else dict()
+        self.redis_db = redis_db if redis_db is not None else Redis()
+        self.local_server = ComputeNode(host=host, port=port,
+                                        functions={'get': self.data.get,
+                                                   'delete': self._delete})
+        for key in data:
+            self.redis_db.sadd(key, self.local_server.url)
+
+    def __del__(self):
+        del self.local_server
+
+    @property
+    def url(self):
+        return self.local_server.url
+
+    def get(self, key):
+        try:
+            return self.data[key]
+        except KeyError:
+            others = self.redis_db.smembers(key)
+            if not others:
+                raise KeyError("Key not found")
+            other = next(iter(others))
+            socket = context.socket(zmq.REQ)
+            socket.connect(other)
+            socket.send(self.local_server.dumps(('get', key)))
+            payload = self.local_server.loads(socket.recv())
+            return payload
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __contains__(self, key):
+        return key in self.data or key in self.redis_db
+
+    def set(self, key, value):
+        if key in self:
+            raise KeyError("Duplicate key found")
+        self.data[key] = value
+        self.redis_db.sadd(key, self.local_server.url)
+
+    def __setitem__(self, key, value):
+        return self.set(key, value)
+
+    def _delete(self, key):
+        del self.data[key]
+
+    def __delitem__(self, key):
+        if key in self.data:
+            del self.data[key]
+
+        socks = []
+        for other in self.redis_db.smembers(key):
+            socket = context.socket(zmq.REQ)
+            socket.connect(other)
+            socket.send(self.local_server.dumps(('delete', key)))
+            socks.append(socket)
+        for sock in socks:
+            payload = self.local_server.loads(sock.recv())
+
+        self.redis_db.delete(key)
+
+    def __iter__(self):
+        local_keys = set(self.data.keys())
+        for key in local_keys:  # Yield our local keys first
+            yield key
+
+        for key in self.redis_db.keys():  # Yield remote keys afterwards
+            if key not in local_keys:
+                yield key
+
+    def __len__(self):
+        # TODO: inspect self.redis_db.info()
+        return sum(1 for i in self)
---
+++ warehouse/tests/test_core.py
@@ -0,0 +1,49 @@
+from shipping import Warehouse
+
+import redis
+
+from contextlib import contextmanager
+
+@contextmanager
+def redis_db(port=6379):
+    r = redis.Redis(db=10, port=port)
+
+    try:
+        yield r
+    finally:
+        for key in r.keys():
+            del r[key]
+
+
+def test_core():
+    with redis_db() as r:
+        a = {'one': 1, 'three': 3}
+        A = Warehouse(data=a, redis_db=r, port=5556)
+
+        b = {'two': 2, 'three': 3}
+        B = Warehouse(data=b, redis_db=r, port=5557)
+
+        try:
+            assert r.smembers('one') == set([A.url])
+            assert r.smembers('three') == set([A.url, B.url])
+            assert A['one'] == 1
+            assert B['two'] == 2
+            assert A['two'] == 2
+            assert B['one'] == 1
+
+            assert 'one' in A
+            assert 'two' not in A.data
+            assert 'two' in A
+
+            assert 'four' not in A
+            A['four'] = 4
+            assert B['four'] == 4
+
+            del B['four']
+            assert 'four' not in A
+
+            assert set(A) == set(['one', 'two', 'three'])
+            assert len(A) == len(B) == 3
+        finally:
+            A.local_server.stop()
+            B.local_server.stop()
